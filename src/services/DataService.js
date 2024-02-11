d4f372e18d9f9621544c2e4df0177126200de2cd
AdrianMartinezGit <adrionmtz02@gmail.com>
2024-02-11 14:33:02 -0800

    Update JS

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -1,27 +1,27 @@
 // Function Definition //
-const addNumberFetch = async () => {
-    const promise = await fetch(`https://allfourone.azurewebsites.net/TwoNumber/AddTwoNumbers/${addNumberOne.value}/${addNumberTwo.value}`);
+const addNumberFetch = async (num1, num2) => {
+    const promise = await fetch(`https://allfourone.azurewebsites.net/TwoNumber/AddTwoNumbers/${num1}/${num2}`);
     const data = await promise.text();
 
     return data;
 }
 
-const questionOneFetch = async () => {
-    const promise = await fetch(`https://allfourone.azurewebsites.net/AskQuestion/AskFirstQuestion/${questionOne.value}`);
+const questionOneFetch = async (question1) => {
+    const promise = await fetch(`https://allfourone.azurewebsites.net/AskQuestion/AskFirstQuestion/${question1}`);
     const data = await promise.text();
 
 	return data;
 }
 
-const questionTwoFetch = async () => {
-    const promise = await fetch(`https://allfourone.azurewebsites.net/AskQuestion/AskSecondQuestion/${questionTwo.value}`);
+const questionTwoFetch = async (question2) => {
+    const promise = await fetch(`https://allfourone.azurewebsites.net/AskQuestion/AskSecondQuestion/${question2}`);
     const data = await promise.text();
 
 	return data;
 }
 
-const compareNumberFetch = async () => {
-    const promise = await fetch(`https://allfourone.azurewebsites.net/GreaterOrLess/GreaterOrLessThan/${compareNumberOne.value}/${compareNumberTwo.value}`);
+const compareNumberFetch = async (num1, num2) => {
+    const promise = await fetch(`https://allfourone.azurewebsites.net/GreaterOrLess/GreaterOrLessThan/${num1}/${num2}`);
     const data = await promise.text();
 
 	return data;
@@ -34,57 +34,57 @@ const MadLibFetch = async () => {
 	return data;
 }
 
-const OddEvenFetch = async () => {
-    const promise = await fetch(`https://allfourone.azurewebsites.net/OddEven/OddOrEven/${numberOE.value}`);
+const OddEvenFetch = async (num) => {
+    const promise = await fetch(`https://allfourone.azurewebsites.net/OddEven/OddOrEven/${num}`);
     const data = await promise.text();
 	
 	return data;
 }
 
-const ReverseNumFetch = async () => {
-    const promise = await fetch(`https://allfourone.azurewebsites.net/ReverseNum/ReverseNum/${reverseNum.value}`);
+const ReverseNumFetch = async (numSeq) => {
+    const promise = await fetch(`https://allfourone.azurewebsites.net/ReverseNum/ReverseNum/${numSeq}`);
     const data = await promise.text();
 
 	return data;
 }
 
-const ReverseABCFetch = async () => {
-    const promise = await fetch(`https://allfourone.azurewebsites.net/reverseABC/reverseABC/${reverseABC.value}`);
+const ReverseABCFetch = async (abcSeq) => {
+    const promise = await fetch(`https://allfourone.azurewebsites.net/reverseABC/reverseABC/${abcSeq}`);
     const data = await promise.text();
 
 	return data;
 }
 
-const EightBallFetch = async () => {
-    const promise = await fetch(`https://allfourone.azurewebsites.net/EightBall/AskQuestion/${ballQuestion.value}`);
+const EightBallFetch = async (question) => {
+    const promise = await fetch(`https://allfourone.azurewebsites.net/EightBall/AskQuestion/${question}`);
     const data = await promise.text();
 
 	return data;
 }
 
-const CategoryCountryFetch = async () => {
-    const promise = await fetch(`https://allfourone.azurewebsites.net/RestaurantPicker/CategoryOrigin/${restaurantPickOne.value}`);
+const CategoryCountryFetch = async (country) => {
+    const promise = await fetch(`https://allfourone.azurewebsites.net/RestaurantPicker/CategoryOrigin/${country}`);
     const data = await promise.text();
 
 	return data;
 }
 
-const CategoryFoodFetch = async () => {
-    const promise = await fetch(`https://allfourone.azurewebsites.net/RestaurantPicker/CategoryFood/${restaurantPickTwo.value}`);
+const CategoryFoodFetch = async (food) => {
+    const promise = await fetch(`https://allfourone.azurewebsites.net/RestaurantPicker/CategoryFood/${food}`);
     const data = await promise.text();
 
 	return data;
 }
 
-const CategoryRatingFetch = async () => {
-    const promise = await fetch(`https://allfourone.azurewebsites.net/RestaurantPicker/CategoryRating/${restaurantPickThree.value}`);
+const CategoryRatingFetch = async (rating) => {
+    const promise = await fetch(`https://allfourone.azurewebsites.net/RestaurantPicker/CategoryRating/${rating}`);
     const data = await promise.text();
 
 	return data;
 }
 
-const NameGreetingFetch = async () => {
-    const promise = await fetch(`https://allfourone.azurewebsites.net/Greeting/GetName/${nameEnter.value}`);
+const NameGreetingFetch = async (name) => {
+    const promise = await fetch(`https://allfourone.azurewebsites.net/Greeting/GetName/${name}`);
     const data = await promise.text();
 
 	return data;
