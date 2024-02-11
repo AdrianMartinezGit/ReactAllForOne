50a38b2a9b58e1b0eb3cc5909a318f4412395a93
AdrianMartinezGit <adrionmtz02@gmail.com>
2024-02-11 13:42:22 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import aquaCube from './assets/cube/cube_aqua.webp'
-import blueCube from './assets/cube/cube_blue.webp'
-import cyanCube from './assets/cube/cube_cyan.webp'
-import greenCube from './assets/cube/cube_green.webp'
-import orangeCube from './assets/cube/cube_orange.webp'
-import pinkCube from './assets/cube/cube_pink.webp'
-import purpleCube from './assets/cube/cube_purple.webp'
-import redCube from './assets/cube/cube_red.webp'
-import whiteCube from './assets/cube/cube_white.webp'
-import yellowCube from './assets/cube/cube_yellow.webp'
+import aquaCube from './assets/cube/cube_aqua.png'
+import blueCube from './assets/cube/cube_blue.png'
+import cyanCube from './assets/cube/cube_cyan.png'
+import greenCube from './assets/cube/cube_green.png'
+import orangeCube from './assets/cube/cube_orange.png'
+import pinkCube from './assets/cube/cube_pink.png'
+import purpleCube from './assets/cube/cube_purple.png'
+import redCube from './assets/cube/cube_red.png'
+import whiteCube from './assets/cube/cube_white.png'
+import yellowCube from './assets/cube/cube_yellow.png'
 
 import './App.css';
 
@@ -15,8 +15,8 @@ function App() {
   return (
     <>
     <div className='parallax-container-home'>
-      <main className='p-4 m-0 w-full'>
-        <h1 className="text-4xl text-white text-center underline font-extrabold">All 4 One - Rubik's Cube Themed!</h1>
+      <main className='p-8 m-0 w-full sm:p-9 md:p-16'>
+        <h1 className="text-4xl md:text-6xl text-white text-center underline font-extrabold pb-4 md:pb-6">All 4 One - Rubik's Cube Themed!</h1>
 
         <div className="flex items-center justify-center pt-6 pb-12">
           <div className='bg-white p-1 rounded-2xl'>
@@ -34,13 +34,13 @@ function App() {
           </div>
         </div>
         
-        <p className="text-center text-white text-4xl font-semibold">Choose a color challenge!</p>
+        <p className="text-center text-white text-4xl md:text-6xl font-semibold">Choose a color challenge!</p>
         
-        <div className="flex items-center justify-center pt-12 pb-12">
+        <div className="flex items-center justify-center pt-12 pb-6">
           <input type="image" src={whiteCube} alt='whiteCube'/>
         </div>
         
-        <p className="text-center text-white text-4xl font-semibold pb-12">Extra Color Challenge</p>
+        <p className="text-center text-white text-4xl md:text-5xl font-semibold pb-12">Extra Color Challenge</p>
       </main>
       </div>
     </>
