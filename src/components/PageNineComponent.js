c03adac5af3604eebcb1cc4e263070e0180c6e3d
AdrianMartinezGit <adrionmtz02@gmail.com>
2024-02-11 15:28:50 -0800

    Padding Component Update

--- src/components/PageNineComponent.js
+++ src/components/PageNineComponent.js
@@ -11,26 +11,28 @@ import { EightBallFetch } from '../services/DataService.js'
 export default function PageNineComponent() {
   return (
     <div className='parallax-container-nine'>
-      <h1>Testing One Two Three!</h1>
-      <h1>Testing One Two Three!</h1>
-      <h1>Testing One Two Three!</h1>
-      <h1>Testing One Two Three!</h1>
-      <h1>Testing One Two Three!</h1>
+      <main className='p-8 m-0 w-full sm:p-9 md:p-16'>
+        <h1>Testing One Two Three!</h1>
+        <h1>Testing One Two Three!</h1>
+        <h1>Testing One Two Three!</h1>
+        <h1>Testing One Two Three!</h1>
+        <h1>Testing One Two Three!</h1>
 
-      <div className='flex justify-center items-end'>
+        <div className='flex justify-center items-end'>
           <div className="grid grid-cols-3 gap-5">
-              <div>
-                  <Link to={'/pageEight'}><input type="image" src={prevButton} className='w-16 min-[320px]:w-24 md:w-32' alt='prevbutton'/></Link>
-              </div>
+            <div>
+              <Link to={'/pageEight'}><input type="image" src={prevButton} className='w-16 min-[320px]:w-24 md:w-32' alt='prevbutton' /></Link>
+            </div>
 
-              <div>
-                  <Link to={'/'}><input type="image" src={homeButton} className='w-16 min-[320px]:w-24 md:w-32' alt='homebutton'/></Link>
-              </div>
-              <div>
-                  <Link to={'/pageTen'}><input type="image" src={nextButton} className='w-16 min-[320px]:w-24 md:w-32' alt='nextbutton'/></Link>
-              </div>        
+            <div>
+              <Link to={'/'}><input type="image" src={homeButton} className='w-16 min-[320px]:w-24 md:w-32' alt='homebutton' /></Link>
+            </div>
+            <div>
+              <Link to={'/pageTen'}><input type="image" src={nextButton} className='w-16 min-[320px]:w-24 md:w-32' alt='nextbutton' /></Link>
+            </div>
           </div>
         </div>
+      </main>
     </div>
   )
 }
